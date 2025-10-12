809f2b4ad1a6bf9d52316ba43b40d4e13266a3c4
Khanikov_Ratmir <hanickovratmir@gmail.com>
2025-10-13 01:47:20 +0300

    basic register

---
+++ app/auth/page.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import React, { useState } from 'react';
+
+export default function Auth() {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+    const [error, setError] = useState("");
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        
+        const emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/
+
+        if (formData.password !== formData.confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
+
+        if (!emailRegex.test(formData.email.toUpperCase())) {
+            setError("Invalid email");
+            return;
+        }
+
+        setError("");
+    }
+
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-8">
+            <h1 className="text-3xl font-bold mb-4">Register</h1>
+            <form onSubmit={handleSubmit}>
+                {error && (<div className="mb-4 text-center bg-red-500 ">{error}</div>)}
+                <input 
+                    name="username" 
+                    placeholder="Username"
+                    onChange={handleChange}
+                    className="border p-2 mb-4 w-full block" 
+                />
+                <input 
+                    name="email" 
+                    type="email" 
+                    placeholder="Email"
+                    onChange={handleChange}
+                    className="border p-2 mb-4 w-full block" 
+                />
+                <input 
+                    name="password" 
+                    type="password" 
+                    placeholder="Password"
+                    onChange={handleChange}
+                    className="border p-2 mb-4 w-full block"
+                />
+                <input 
+                    name="confirmPassword" 
+                    type="password" 
+                    placeholder="Confirm password"
+                    onChange={handleChange}
+                    className="border p-2 mb-4 w-full block"
+                />
+
+                <button type="submit" className="bg-blue-500 text-white p-2 rounded w-full">Register</button>
+            </form>
+        </main>
+        
+    )
+}
